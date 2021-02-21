014b400adba4ac46f16aa0c353869263b9bd4a0f
rohanverma264 <79327752+rohanverma264@users.noreply.github.com>
2021-02-21 13:19:44 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,7 @@
+function keyCode(key) {
+    var input = document.getElementById('screen');
+    var result = document.getElementsByClassName('grid-items').innerHTML;
+    input = result;
+    input.innerHTML= result;
+    
+}
