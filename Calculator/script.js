ea9d836ddd6864ec949ccc4f4a39eaa859995256
rohanverma264 <79327752+rohanverma264@users.noreply.github.com>
2021-02-24 03:20:19 +0530

    Add files via upload

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,7 +1,24 @@
-function keyCode(key) {
-    var input = document.getElementById('screen');
-    var result = document.getElementsByClassName('grid-items').innerHTML;
-    input = result;
-    input.innerHTML= result;
-    
+var input = document.getElementById('screen');
+var buttons = document.getElementsByTagName('button');
+var scrValue = '';
+
+for(items of buttons){
+    items.addEventListener('click',function(){
+        buttonText = this.innerText;
+        console.log(buttonText);
+        
+        
+        if(buttonText == 'C'){
+            input.value = '';
+            scrValue = '';
+        }
+        else if(buttonText == '='){
+            input.value = eval(scrValue);
+        }
+        
+        else{
+            scrValue += buttonText;
+            input.value = scrValue;
+        }
+    })
 }
